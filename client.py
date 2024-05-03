83cf3db52710c8d1b669fa6893811ecc4a8d3e2a
Asmita Bhandare <20106482@tafe.wa.edu.au>
2024-05-03 13:49:47 +0800

    added client.py

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 1234))
+print("Connected to Magic-8-ball server")
+
+while True:
+    question = input("Ask your question: \n")
+    if question.lower() == 'quit':
+        print("Have a nice day!")
+        break
+
+    client_socket.send(question.lower().encode('utf-8'))
+
+    answer = client_socket.recv(1024).decode('utf-8')
+    print(answer)
+
+client_socket.close()
